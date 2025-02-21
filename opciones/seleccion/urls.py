a9115b602eee76358e6933c4b4ae1733dd6d971b
gafarina <gaston.farina@gmail.com>
2025-02-21 14:46:03 -0300

    primera tabla

--- opciones/seleccion/urls.py
+++ opciones/seleccion/urls.py
@@ -4,6 +4,6 @@ from .import views
 urlpatterns = [
     #
     #path('', views.stockPicker, name = 'stockpicker'),
-    # Este es el path de la applicacion
-    #path('stocktracker/', views.stockTracker, name = 'stocktracker'),
+    #Este es el path de la applicacion que es la base
+    path('', views.seleccion, name = 'seleccion'),
 ]
--- opciones/seleccion/views.py
+++ opciones/seleccion/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def seleccion(request):
+    #stock_picker = tickers_nifty50()
+    #print(stock_picker)
+    # va a llamar el html en el template
+    return render(request, 'seleccion/base.html')
